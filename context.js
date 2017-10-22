c0198f06bf9a34e2ed10916586a7358aa50c6a45
larakallab <larakallab@gmail.com>
2017-10-22 13:01:00 +0200

    Add files via upload

---
+++ context.js
@@ -0,0 +1,11 @@
+{
+	"@context": {
+		"h2g": "http://localhost:5000/h2g.owl",
+		"http": "http://www.w3.org/2011/http#",
+		"http-methods":"http://www.w3.org/2011/http-methods",
+		"cnt": "http://www.w3.org/2011/content#",
+		"dct": "http://purl.org/dc/terms",
+		"owl": "http://www.w3.org/2002/07/owl#",
+		"rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+	}
+}
