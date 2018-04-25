a1383739b7f8d45e093c20e0288bbef6a76d7ce8
Lara <lkallab@nobatek.com>
2018-04-25 18:19:30 +0200

    services description

--- server.py
+++ server.py
@@ -31,18 +31,25 @@ from services.heatengcons import heatengcons
 
 from servicesDesc.ATC1 import getairtemp
 from servicesDesc.ATC2 import collectairtemp
+from servicesDesc.ATC3 import collairtemp
 from servicesDesc.CTC1 import getclimtemp
 from servicesDesc.CTC2 import collectclimtemp
+from servicesDesc.CTC3 import collclimtemp
 from servicesDesc.MVD1 import missvaldetection
 from servicesDesc.MVD2 import missvaldet
+from servicesDesc.MVD3 import missdet
 from servicesDesc.MVI1 import missvalinterpolation
 from servicesDesc.MVI2 import missvalint
+from servicesDesc.MVI3 import missint
 from servicesDesc.OVD1 import outvaldetection
 from servicesDesc.OVD2 import outvaldet
+from servicesDesc.OVD3 import outdet
 from servicesDesc.OVI1 import outvalinterpolation
 from servicesDesc.OVI2 import outvalint
+from servicesDesc.OVI3 import outint
 from servicesDesc.BEDP1 import predheatengcons
 from servicesDesc.BEDP2 import predheateng
+from servicesDesc.BEDP3 import predheat
 
 from static import static
 from hateoas import hateoas
@@ -75,31 +82,50 @@ app.register_blueprint(getairtemp, url_prefix='/resource/getairtemperature')
 app.register_blueprint(getairtemp, url_prefix='/resourceDescription')
 app.register_blueprint(collectairtemp, url_prefix='/resource/collectairtemperature')
 app.register_blueprint(collectairtemp, url_prefix='/resourceDescription')
+app.register_blueprint(collairtemp, url_prefix='/resource/collairtemp')
+app.register_blueprint(collairtemp, url_prefix='/resourceDescription')
+
 app.register_blueprint(getclimtemp, url_prefix='/resource/getclimtemperature')
 app.register_blueprint(getclimtemp, url_prefix='/resourceDescription')
 app.register_blueprint(collectclimtemp, url_prefix='/resource/collectclimtemperature')
 app.register_blueprint(collectclimtemp, url_prefix='/resourceDescription')
+app.register_blueprint(collclimtemp, url_prefix='/resource/collclimtemp')
+app.register_blueprint(collclimtemp, url_prefix='/resourceDescription')
+
 app.register_blueprint(missvaldetection, url_prefix='/resource/missvaldetection')
 app.register_blueprint(missvaldetection, url_prefix='/resourceDescription')
 app.register_blueprint(missvaldet, url_prefix='/resource/missvaldet')
 app.register_blueprint(missvaldet, url_prefix='/resourceDescription')
+app.register_blueprint(missdet, url_prefix='/resource/missdet')
+app.register_blueprint(missdet, url_prefix='/resourceDescription')
+
 app.register_blueprint(outvaldetection, url_prefix='/resource/outvaldetection')
 app.register_blueprint(outvaldetection, url_prefix='/resourceDescription')
 app.register_blueprint(outvaldet, url_prefix='/resource/outvaldet')
 app.register_blueprint(outvaldet, url_prefix='/resourceDescription')
+app.register_blueprint(outdet, url_prefix='/resource/outdet')
+app.register_blueprint(outdet, url_prefix='/resourceDescription')
+
 app.register_blueprint(missvalinterpolation, url_prefix='/resource/missvalinterpolation')
 app.register_blueprint(missvalinterpolation, url_prefix='/resourceDescription')
 app.register_blueprint(missvalint, url_prefix='/resource/missvalint')
 app.register_blueprint(missvalint, url_prefix='/resourceDescription')
+app.register_blueprint(missint, url_prefix='/resource/missint')
+app.register_blueprint(missint, url_prefix='/resourceDescription')
+
 app.register_blueprint(outvalinterpolation, url_prefix='/resource/outvalinterpolation')
 app.register_blueprint(outvalinterpolation, url_prefix='/resourceDescription')
 app.register_blueprint(outvalint, url_prefix='/resource/outvalint')
 app.register_blueprint(outvalint, url_prefix='/resourceDescription')
+app.register_blueprint(outint, url_prefix='/resource/outint')
+app.register_blueprint(outint, url_prefix='/resourceDescription')
+
 app.register_blueprint(predheatengcons, url_prefix='/resource/predheatengcons')
 app.register_blueprint(predheatengcons, url_prefix='/resourceDescription')
 app.register_blueprint(predheateng, url_prefix='/resource/predheateng')
 app.register_blueprint(predheateng, url_prefix='/resourceDescription')
-
+app.register_blueprint(predheat, url_prefix='/resource/predheat')
+app.register_blueprint(predheat, url_prefix='/resourceDescription')
 
 app.register_blueprint(static, url_prefix='')
 app.register_blueprint(static, url_prefix='')
